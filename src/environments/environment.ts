3df433844c843d35758a19cf06d50504061066b7
ManuelChan <99447411+ManuelinX@users.noreply.github.com>
2022-11-20 02:24:46 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   pokemonApi: 'https://pokeapi.co/api/v2/',
   digimonApi: 'https://www.digi-api.com/api/v1/digimon',
   moviesApi: 'https://imdb-api.com/api',
-  cardApis: 'https://api.pokemontcg.io/v2/cards?q=name:gardevoir',
+  cardApi: 'https://api.pokemontcg.io/v2/cards?q=name:gardevoir',
 };
 
 /*
