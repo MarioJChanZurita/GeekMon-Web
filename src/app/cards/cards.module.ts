d0b791118ea1e9593d98970a71b7a491bf91a9ea
ManuelChan <99447411+ManuelinX@users.noreply.github.com>
2022-11-20 02:30:32 -0600

    Cards ngonInit
    Se agrego el metodo onInit en cards

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './components/cards/cards.component';
 
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SharedService } from 'src/app/shared/services/shared.service';
+
 
 @NgModule({
   declarations: [
@@ -14,4 +19,40 @@ import { CardsComponent } from './components/cards/cards.component';
     CardsRoutingModule
   ]
 })
-export class CardsModule { }
+export class CardsModule {
+  pokemons : any[] = [];
+  constructor(private sharedService: SharedService,private http:HttpClient) {}
+  
+  ngOnInit(): void {
+    this.isLoading = true;
+
+   
+    
+    let variable = {};
+    this.getC().subscribe(resp => {
+      this.pokemons.push(resp);
+      
+      
+    },)
+   
+    
+
+  }
+
+  getC(){
+    return this.http.get(this.sharedService.cardsApi)
+  }
+
+
+
+  getCards(noPage: string) {
+    this.Pokemons && this.Pokemons.unsubscribe();
+    this.Pokemons = this.sharedService.getCards().subscribe({
+      
+      error: () => {
+        this.pokemons = [];
+        this.isLoading = false;
+      },
+    });
+  }
+}
