09d19b17695d1db6daf2982695a60a7114e78ab4
ManuelChan <99447411+ManuelinX@users.noreply.github.com>
2022-11-20 02:32:11 -0600

    Corrigio error en cards

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -4,55 +4,17 @@ import { CommonModule } from '@angular/common';
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './components/cards/cards.component';
 
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { SharedService } from 'src/app/shared/services/shared.service';
 
 
 @NgModule({
   declarations: [
-    CardsComponent
+    CardsComponent,
+
   ],
   imports: [
     CommonModule,
     CardsRoutingModule
   ]
 })
-export class CardsModule {
-  pokemons : any[] = [];
-  constructor(private sharedService: SharedService,private http:HttpClient) {}
-  
-  ngOnInit(): void {
-    this.isLoading = true;
+export class CardsModule { }
 
-   
-    
-    let variable = {};
-    this.getC().subscribe(resp => {
-      this.pokemons.push(resp);
-      
-      
-    },)
-   
-    
-
-  }
-
-  getC(){
-    return this.http.get(this.sharedService.cardsApi)
-  }
-
-
-
-  getCards(noPage: string) {
-    this.Pokemons && this.Pokemons.unsubscribe();
-    this.Pokemons = this.sharedService.getCards().subscribe({
-      
-      error: () => {
-        this.pokemons = [];
-        this.isLoading = false;
-      },
-    });
-  }
-}
